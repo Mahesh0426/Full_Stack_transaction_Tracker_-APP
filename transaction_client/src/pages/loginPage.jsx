af85bfdfaf559096dcc140ba93beadeef7049a0d
Mahesh792 <mawhesjung143@gmail.com>
2025-01-22 10:01:42 +1100

    make form responsive

--- transaction_client/src/pages/loginPage.jsx
+++ transaction_client/src/pages/loginPage.jsx
@@ -1,33 +1,41 @@
-import { Col, Container, Row, Stack } from "react-bootstrap"
+import { Col, Container, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import LoginForm from "../components/loginForm";
 
 const LoginPage = () => {
-  return ( 
-    <Container>
-      <Row className="d-flex align-items-center justiy-content-center vh-100">
-        <Col>
-          <Stack className="shadow-lg border rounded p-4">
+  return (
+    <Container
+      fluid
+      className="d-flex align-items-center justify-content-center vh-100"
+    >
+      <Row className="w-100">
+        {/* Welcome Section */}
+        <Col
+          xs={12}
+          md={6}
+          className="d-flex justify-content-center mb-4 mb-md-0"
+        >
+          <Stack className="shadow-lg border rounded p-4 text-center">
             <h1>Welcome Back!!</h1>
-            <p>Manage yur income and expenses</p>
-            <p>Track Your Fincance</p>
+            <p>Manage your income and expenses</p>
+            <p>Track Your Finance</p>
           </Stack>
         </Col>
 
-        {/* Login Form */}
-        <Col>
+        {/* Login Form Section */}
+        <Col xs={12} md={6} className="d-flex justify-content-center">
           <Stack className="shadow-lg border rounded p-4">
-            <h1>Login</h1>
-
-            {/* Login Form */}
-            <LoginForm/>
-
-            <p>Don&apos;t have account?</p><Link to="/signup">Sign Up</Link>
+            <h1 className="text-center">Login</h1>
+            <LoginForm />
+            <div className="text-center mt-3">
+              <p>Don&apos;t have an account?</p>
+              <Link to="/signup">Sign Up</Link>
+            </div>
           </Stack>
         </Col>
       </Row>
     </Container>
-   );
-}
- 
+  );
+};
+
 export default LoginPage;
--- transaction_client/src/pages/signupPage.jsx
+++ transaction_client/src/pages/signupPage.jsx
@@ -1,33 +1,41 @@
-import { Col, Container, Row, Stack } from "react-bootstrap"
+import { Col, Container, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import SignupForm from "../components/signupForm";
 
 const SignupPage = () => {
-  return ( 
-    <Container>
-      <Row className="d-flex align-items-center justiy-content-center vh-100">
-        <Col>
-          <Stack className="shadow-lg border rounded p-4">
+  return (
+    <Container
+      fluid
+      className="d-flex align-items-center justify-content-center vh-100"
+    >
+      <Row className="w-100">
+        {/* Welcome Section */}
+        <Col
+          xs={12}
+          md={6}
+          className="d-flex justify-content-center mb-4 mb-md-0"
+        >
+          <Stack className="shadow-lg border rounded p-4 text-center">
             <h1>Join Our App!!</h1>
-            <p>Manage yur income and expenses</p>
-            <p>Track Your Fincance</p>
+            <p>Manage your income and expenses</p>
+            <p>Track Your Finance</p>
           </Stack>
         </Col>
 
-        {/* SignUp Form */}
-        <Col>
+        {/* Signup Form Section */}
+        <Col xs={12} md={6} className="d-flex justify-content-center">
           <Stack className="shadow-lg border rounded p-4">
-            <h1>Sign Up</h1>
-
-            {/* SignUp Form */}
+            <h1 className="text-center">Sign Up</h1>
             <SignupForm />
-
-            <p>Already have account?</p><Link to="/login">Login</Link>
+            <div className="text-center mt-3">
+              <p>Already have an account?</p>
+              <Link to="/login">Login</Link>
+            </div>
           </Stack>
         </Col>
       </Row>
     </Container>
-   );
-}
- 
+  );
+};
+
 export default SignupPage;
